0cc25004c2ecd1ab5eae3915244963b7a1d1d181
Aditya <adityawantstoread@gmail.com>
2023-02-09 12:29:49 +0800

    Finished doing the basic grid and adding a basic 16x16 hover function, however, need to add a variable size function, but good progress

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 
 let container = document.querySelector('.container');
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16. 1fr)';
-container.appendChild(board);
+let instructions = document.createElement('instructions');
+let body = document.querySelector('body');
+let resetbtn = document.querySelector('.resetbtn');
+body.appendChild(instructions);
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16. 1fr)';
+
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    
+    square.style.border = "thin solid black";
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+        
+    })
+    
+   container.appendChild(square);
+}
+
+
+
+
+
+
 
