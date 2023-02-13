38df0adf107565445fce2f3e1f803cb997d96477
Aditya <adityawantstoread@gmail.com>
2023-02-13 11:47:52 +0800

    Final project pushed with changes to mouse colour changing, alongside fixing the reset button so now the divs refresh and a whole new set of divs are generated

--- script.js
+++ script.js
@@ -3,23 +3,48 @@ let container = document.querySelector('.container');
 let instructions = document.createElement('instructions');
 let body = document.querySelector('body');
 let resetbtn = document.querySelector('.resetbtn');
+let choicebtn = document.querySelector('.choicebtn');
 body.appendChild(instructions);
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-container.style.gridTemplateRows = 'repeat(16. 1fr)';
 
-
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
+function fillBoard(size) {
+    let container = document.querySelector('.container');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
     
-    square.style.border = "thin solid black";
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
+  
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+    
         
-    })
+        square.style.border = "thin solid black";
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+            
+        })
+        
+        resetbtn.addEventListener('click', () => {
+            square.style.backgroundColor = 'white';
+        })
     
-   container.appendChild(square);
+       container.appendChild(square);
+
+       if (amount > 10000) {
+        return "You can only have 100 squares maximum on each side of the grid!";
+       }
+    }
+    
 }
 
+function changeSize(input) {
+    fillBoard(input);
+}
+
+
+console.log(fillBoard(16))
 
 
 
